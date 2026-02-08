a861b55df9da4760a6e3d6a5fec859b5056982b8
Rukayyat Adelekan <132057787+rukies01@users.noreply.github.com>
2026-02-08 14:06:14 +0100

    Update Solution.sql

--- Day 3: Multi-Table Joins/Solution.sql
+++ Day 3: Multi-Table Joins/Solution.sql
@@ -1,1 +1,46 @@
+1. Show order number, product name, category, and subcategory.
 
+SELECT 
+	S.SalesOrderNumber,
+	P.EnglishProductName,
+	C.EnglishProductCategoryName,
+	E.EnglishProductSubcategoryName
+FROM FactInternetSales S
+LEFT JOIN DimProduct P
+ON S.ProductKey = P.ProductKey
+LEFT JOIN DimProductSubcategory E
+ON P.ProductSubcategoryKey = E.ProductSubcategoryKey
+LEFT JOIN DimProductCategory C
+ON E.ProductCategoryKey = C.ProductCategoryKey
+
+
+
+--2. Find total sales amount per customer.
+SELECT 
+	CONCAT(C.FirstName,' ', COALESCE(C.MiddleName+' ', ''), C.LastName) AS CustomerFullName,
+	SUM(S.SalesAmount) AS TotalSales
+FROM FactInternetSales S
+LEFT JOIN DimCustomer C
+ON S.CustomerKey = C.CustomerKey
+GROUP BY CONCAT(C.FirstName,' ', COALESCE(C.MiddleName+' ', ''), C.LastName)  
+ORDER BY SUM(S.SalesAmount) desc
+
+--Nichole Nara has the highest total sales
+
+--3. Identify top 10 customers by total sales value
+WITH TSales AS
+(
+SELECT
+	C.CustomerKey,
+	CONCAT(C.FirstName,' ', COALESCE(C.MiddleName+' ', ''), C.LastName) AS CustomerFullName,
+	SUM(S.SalesAmount) AS TotalSales
+FROM FactInternetSales S
+LEFT JOIN DimCustomer C
+ON S.CustomerKey = C.CustomerKey
+GROUP BY 
+	C.CustomerKey,
+	CONCAT(C.FirstName,' ', COALESCE(C.MiddleName+' ', ''), C.LastName) 
+)
+SELECT TOP 10 *
+FROM TSales
+ORDER BY TotalSales DESC
