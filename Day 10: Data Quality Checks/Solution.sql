5b203c31e3af650fb20962906d7b5a3817e1cb73
Rukayyat Adelekan <132057787+rukies01@users.noreply.github.com>
2026-02-18 14:11:38 +0100

    Update Solution.sql

--- Day 10: Data Quality Checks/Solution.sql
+++ Day 10: Data Quality Checks/Solution.sql
@@ -1,1 +1,33 @@
 
+1. Find orders with missing customer information.
+SELECT 
+	C.CustomerKey,
+	CONCAT(C.FirstName,' ', COALESCE(C.MiddleName+' ', ''), C.LastName) AS CustomerFullName,
+	F.SalesOrderNumber,
+	F.SalesOrderLineNumber
+From FactInternetSales F
+LEFT JOIN DimCustomer C
+ON F.CustomerKey = C.CustomerKey
+Where C.CustomerKey is Null
+
+
+2. Detect duplicate records.
+SELECT 
+    SalesOrderNumber,
+    SalesOrderLineNumber,
+    COUNT(*) AS RecordCount
+FROM FactInternetSales
+GROUP BY 
+    SalesOrderNumber,
+    SalesOrderLineNumber
+HAVING COUNT(*) > 1
+
+3. Identify negative or zero values where they shouldn’t exist.
+SELECT 
+	SalesAmount,
+	UnitPrice,
+	OrderQuantity
+FROM FactInternetSales
+Where SalesAmount <= 0
+	OR UnitPrice <= 0
+	OR OrderQuantity <= 0
